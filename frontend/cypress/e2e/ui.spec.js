d76ddedba00563c1e3b26eb584a6ab25feb77d92
Sashank <sashank.k04@gmail.com>
2025-04-23 13:32:27 -0400

    Ui Spec NA

--- frontend/cypress/e2e/ui.spec.js
+++ frontend/cypress/e2e/ui.spec.js
@@ -1,32 +1,25 @@
 // frontend/cypress/e2e/ui.spec.js
 
 describe("Resume Site Smoke Tests", () => {
-    
     beforeEach(() => {
-      // 1) spy on the HTTP-trigger call (catches the code query-param too)
       cy.intercept({
         method: "GET",
         path: "/api/http_trigger*"
       }).as("getCount");
   
-      // 2) visit the root
       cy.visit("/");
-  
-      // 3) wait for our logic.js fetch to complete
-      cy.wait("@getCount");
-    });
-  
-    it("loads the header correctly", () => {
-      cy.get("header h1")
-        .should("contain.text", "Sashank Konathala");
+      // wait for the actual Azure Function call
+      cy.wait("@getCount").its("response.statusCode").should("eq", 200);
     });
   
     it("displays and logs the visitor counter", () => {
       cy.get("#visitor-counter")
         .should("exist")
+        // make sure we didn’t fall into the catch() setting “N/A”
+        .should("not.have.text", "N/A")
         .invoke("text")
         .then((text) => {
-          cy.log(" Visitor counter after API call:", text);
+          cy.log("🚀 Visitor counter after API call:", text);
           expect(text).to.match(/^\d+$/);
         });
     });
