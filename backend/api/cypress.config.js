f25f5f5baebd4814fa4b7110bd37ec8e34609e5b
Sashank <sashank.k04@gmail.com>
2025-04-23 00:35:13 -0400

    config cypress

---
+++ backend/api/cypress.config.js
@@ -0,0 +1,8 @@
+// Backend/api/cypress.config.js
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: 'cypress/e2e/**/*.spec.js'
+  }
+});
---
+++ frontend/cypress.config.js
@@ -0,0 +1,9 @@
+// Frontend/cypress.config.js
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: 'cypress/e2e/**/*.spec.js',
+    baseUrl: 'http://localhost:5000'
+  }
+});
